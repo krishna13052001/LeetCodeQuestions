315020f74de833150483ee1159f83c39ea172983
Krishna <sathya13052001krishna@gmail.com>
2022-01-17 08:46:53 +0530

    Time: 51 ms (33.86%), Space: 14.2 MB (97.44%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,13 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        if(len(nums) == 1):
+            return [nums.copy()]
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            perms = self.permute(nums)
+            for i in perms:
+                i.append(n)
+            res.extend(perms)
+            nums.append(n)
+        return res
