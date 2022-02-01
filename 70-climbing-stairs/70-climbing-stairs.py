bd941c6f31ee6e2304e507d0363329994f0b1aa3
Krishna <sathya13052001krishna@gmail.com>
2022-02-01 17:20:42 +0530

    Time: 70 ms (5.55%), Space: 13.8 MB (97.68%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,5 +1,5 @@
 class Solution:
-    dp = [-1]*46
+    """dp = [-1]*46
     dp[0]=1
     dp[1]=1
     def findAns(self,n):
@@ -9,6 +9,13 @@ class Solution:
             return 0
         else:
             self.dp[n] = self.findAns(n-1) + self.findAns(n-2)
-            return self.dp[n]
+            return self.dp[n]"""
+    
     def climbStairs(self, n: int) -> int:
-        return self.findAns(n)
+        # return self.findAns(n)
+        prev2,prev1 = 1,1
+        for i in range(2,n+1):
+            curr = prev1+prev2
+            prev2 = prev1
+            prev1=curr
+        return prev1
