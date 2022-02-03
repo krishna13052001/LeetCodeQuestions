3b8cd859ae1bda03cf20caec4748c883fd24707b
Krishna <sathya13052001krishna@gmail.com>
2022-02-03 10:09:22 +0530

    Time: 72 ms (29.83%), Space: 13.9 MB (96.19%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -1,4 +1,16 @@
-from math import sqrt
+import sys
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return int(sqrt(x))
+        low = 0
+        ans = 0
+        end = sys.maxsize//2
+        while(low<=end):
+            mid = low+(end-low)//2
+            # print(mid,end)
+            if(mid*mid<=x):
+                ans = mid
+                low = mid+1
+            else:
+                end = mid -1
+        return ans
+            
