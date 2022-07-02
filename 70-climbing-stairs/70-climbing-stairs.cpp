9ceb58965e145d6798eace18aecf1dc0b4fbd441
Krishna <sathya13052001krishna@gmail.com>
2022-07-03 01:49:33 +0530

    Time: 0 ms (100.00%), Space: 6 MB (56.37%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1 || n==0)
+            return 1;
+        if(n==2)
+            return 2;
+        int j = 1,i = 2,newnumber = 0;
+        while(n>2){
+            newnumber = i + j;
+            j = i;
+            i = newnumber;
+            n--;
+        }
+        return i;
+    }
+};
