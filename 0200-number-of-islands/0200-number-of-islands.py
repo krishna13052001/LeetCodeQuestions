11bf82d0571d81fe1451f220d07db913fb847e64
Krishna <sathya13052001krishna@gmail.com>
2024-05-03 12:15:42 +0530

    Time: 218 ms (95.56%), Space: 18.9 MB (55.50%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -16,26 +16,17 @@ class Solution:
             pass
 
     def numIslands(self, grid: List[List[str]]) -> int:
-        # n = int(input())
-        # mat = []
         n = len(grid)
         m = len(grid[0])
         mat = grid
         count_1 = 0
-        # for i in range(n):
-        #     arr = list(map(int,input().split()))
-        #     mat.append(arr)
-        # print(mat)
         count = 0
         i,j = 0,0
         while(i < n and j < m):
             if(mat[i][j] == '1'):
-                # print("Hi")
-                # print(i,i,mat[i][j])
                 count += 1
                 self.find(mat,i,j)
             j += 1
-            # print("Jvalue ",j,m)
             if(j >= m):
                 i += 1
                 j = 0
