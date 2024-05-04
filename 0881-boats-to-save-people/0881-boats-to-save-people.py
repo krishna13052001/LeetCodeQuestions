29e470f665e7e0bff375ed6681826e53f8730896
Krishna <sathya13052001krishna@gmail.com>
2024-05-04 13:28:58 +0530

    Time: 419 ms (5.01%), Space: 23.8 MB (8.54%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -0,0 +1,12 @@
+from collections import deque
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        stack = deque(sorted(people))
+        print(stack)
+        count = 0
+        while(stack):
+            end = stack.pop()
+            count += 1
+            if(stack and end + stack[0]<=limit):
+                stack.popleft()
+        return count
