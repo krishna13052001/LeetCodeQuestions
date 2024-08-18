1c748708f42a853a571503416d030f6a231ac050
Krishna <sathya13052001krishna@gmail.com>
2024-08-18 16:48:31 +0530

    Time: 94 ms (96.1%), Space: 17.6 MB (40.55%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,13 +1,9 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        goal = len(nums)-1
-        idx = goal - 1
-        count = 0
-
-        while goal > 0:
-            for i in range(goal-1,-1,-1):
-                if i+nums[i] >= goal:
-                    idx = min(idx,i)
-            goal = idx
-            count += 1
+        mx,count,curr=0,0,0
+        for i in range(len(nums)-1):
+            if i + nums[i] > mx: 
+                mx = i + nums[i]
+            if i == curr: 
+                curr, count = mx, count+1 
         return count
