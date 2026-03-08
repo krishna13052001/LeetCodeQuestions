805400010efa7d78603eeef1ceade517213e1d32
Krishna <sathya13052001krishna@gmail.com>
2026-03-08 11:42:05 +0530

    Time: 3 ms (85.75%), Space: 22.2 MB (97.52%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -7,7 +7,6 @@ class Solution:
         visited = [[False] * cols for _ in range(rows)]
         def dfs(row, col):
             directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-            print(row, col)
             visited[row][col] = True
             for dr, dc in directions:
                 nr = row + dr
@@ -16,17 +15,13 @@ class Solution:
                     dfs(nr, nc)
         for rdx in range(rows):
             if board[rdx][0] == "O" and not visited[rdx][0]:
-                print("1")
                 dfs(rdx, 0)
             if board[rdx][-1] == "O" and not visited[rdx][-1]:
-                print("2", rdx, cols - 1)
                 dfs(rdx, cols - 1)
         for cdx in range(cols):
             if board[0][cdx] == "O" and not visited[0][cdx]:
-                print("3")
                 dfs(0, cdx)
             if board[-1][cdx] == "O" and not visited[-1][cdx]:
-                print("4")
                 dfs(rows - 1, cdx)
         for row in range(rows):
             for col in range(cols):
