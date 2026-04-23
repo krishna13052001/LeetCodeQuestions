b1fa4f606c393fe2fa1e11027c7631e3da481fbc
Krishna <sathya13052001krishna@gmail.com>
2026-04-23 16:34:24 +0530

    Time: 0 ms (100%), Space: 19.3 MB (56.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,18 +1,12 @@
 class Solution:
-    dp = {}
     def climbStairs(self, n: int) -> int:
-        if n <= 1:
-            return 1
-        left, right = 0 ,0
-        if self.dp.get(n-1,-1) != -1:
-            left = self.dp[n-1]
-        else:
-            left = self.climbStairs(n-1)
-            self.dp[n-1] = left
-        if self.dp.get(n-2,-1) != -1:
-            right = self.dp[n-2]
-        else:
-            right = self.climbStairs(n-2)
-            self.dp[n-1] = right
-        return left + right
-        
+        if n <= 2:
+            return n
+
+        dp = [0] * (n + 1)
+        dp[1], dp[2] = 1, 2
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
