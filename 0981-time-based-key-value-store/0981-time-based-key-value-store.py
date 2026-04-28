ef9e362611780ef696f444660ec8fad63022d9a1
Krishna <sathya13052001krishna@gmail.com>
2026-04-28 09:43:40 +0530

    Time: 68 ms (89.53%), Space: 69.1 MB (52.91%) - LeetHub

---
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+from bisect import bisect_right
+class TimeMap:
+
+    def __init__(self):
+        self.store = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.store[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.store:
+            return ""
+        times = self.store[key]
+        idx = bisect_right(times, (timestamp, chr(127)))
+        return times[idx - 1][1] if idx else ""
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
